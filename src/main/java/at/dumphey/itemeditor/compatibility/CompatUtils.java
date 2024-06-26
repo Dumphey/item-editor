330f55d5d09bac16391b835811013d2984fe70c0
Dumphey <141498294+Dumphey@users.noreply.github.com>
2024-06-26 13:59:29 +0200

    fix 1.21

--- src/main/java/at/dumphey/itemeditor/compatibility/CompatUtils.java
+++ src/main/java/at/dumphey/itemeditor/compatibility/CompatUtils.java
@@ -1,10 +1,12 @@
 package at.dumphey.itemeditor.compatibility;
 
 import at.dumphey.itemeditor.ItemEditor;
+import org.apache.commons.lang.StringUtils;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.util.StringUtil;
 
 import java.util.logging.Level;
 
@@ -22,15 +24,23 @@ public class CompatUtils {
     }
 
     private void parseVersion() {
-        String versionString = Bukkit.getServer().getVersion();
-        String mc = versionString.substring(versionString.indexOf("MC: ") + 4);
-        mc = mc.substring(2, mc.indexOf(')'));
-        String[] parts = mc.split("\\.");
-        majorVersion = Integer.parseInt(parts[0]);
-        minorVersion = Integer.parseInt(parts[1]);
-        ItemEditor.getInstance()
-                .getLogger()
-                .log(Level.INFO, "Detected spigot 1." + majorVersion + "." + minorVersion);
+        try {
+            String versionString = Bukkit.getServer().getVersion();
+            String mc = versionString.substring(versionString.indexOf("MC: ") + 4);
+            mc = mc.substring(2, mc.indexOf(')'));
+            String[] parts = mc.split("\\.");
+            majorVersion = Integer.parseInt(parts[0]);
+            if (parts.length > 1) {
+                minorVersion = Integer.parseInt(parts[1]);
+            } else {
+                minorVersion = 0;
+            }
+            ItemEditor.getInstance()
+                    .getLogger()
+                    .log(Level.INFO, "Detected minecraft 1." + majorVersion + "." + minorVersion);
+        } catch (Exception e) {
+            ItemEditor.getInstance().getLogger().log(Level.SEVERE, "Failed to parse version", e);
+        }
     }
 
     public static boolean isAir(ItemStack itemStack) {
